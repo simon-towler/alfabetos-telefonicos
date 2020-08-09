5d422980b9b66d615c58aa43c3f46f781bc61d5f
simon-towler <simon.towler@icloud.com>
2020-08-09 18:21:11 +0100

    Initial data structure of word for each letter

---
+++ AlfabetosTelefonicos.java
@@ -0,0 +1,33 @@
+class AlfabetosTelefonicos {
+  String[] alfabetoTelefonico = {
+    "Alicante",
+    "Bilbao",
+    "Cadiz",
+    "Dinamarca",
+    "Espana",
+    "Francia",
+    "Girona",
+    "Huelva",
+    "Italia",
+    "Jaen",
+    "Kilo",
+    "Lugo",
+    "Llamar",
+    "Madrid",
+    "Navarra",
+    "Oviedo",
+    "Portugal",
+    "Queso",
+    "Roma",
+    "EreDoble",
+    "Sevilla",
+    "Toledo",
+    "Unico",
+    "Valencia",
+    "UveDoble",
+    "Equis",
+    "Yugoslavia",
+    "Zaragosa"
+  };
+
+}
